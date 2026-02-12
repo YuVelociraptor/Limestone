720e5e1cde9ad4b97af632468755dcf428088012
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2026-02-12 20:35:16 +0900

    read, print, loop of REPL

--- src/main/java/jp/complexalpha/Main.java
+++ src/main/java/jp/complexalpha/Main.java
@@ -3,5 +3,14 @@ package jp.complexalpha;
 public class Main {
     static void main() {
 
+        while (true){
+            String s = IO.readln();
+            IO.println(s);
+            IO.println( s.length());
+
+            if (s.equals("exit")){
+                break;
+            }
+        }
     }
 }
